07c41ade6e371a430128d2b10c4b891b6c7c68a7
thalesousa <thalestjsb@gmail.com>
2020-08-05 18:10:12 -0300

    Aula 3

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,38 +3,55 @@ import React from "react";
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css'
+import api from "../../services/api";
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection(){
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
         <img
-          src="https://avatars1.githubusercontent.com/u/37390511?s=460&u=8df0e51b5c91f47639142f1c12f38291ef5d7fb9&v=4"
-          alt="Thales Sousa"
+          src={teacher.avatar}
+          alt={teacher.name}
         />
         <div>
-          <strong>Thales Sousa</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
       <p>
-        Entusiasta das melhores tecnologias e química avançada.
-        <br /> <br />
-        Apaixonado por explodir coisas em laboratórios e por a vida das pessoas
-        através de experiencias. mais de 200.000 pessoas já passaram por uma das
-        minhas explosões.
+        {teacher.bio}
       </p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
